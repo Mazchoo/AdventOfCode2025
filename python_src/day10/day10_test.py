9e5022ae1ca3aa2c8c043bdcecc455c352adf7b8
Matthew Mitchell <mazchoo@gmail.com>
2026-01-16 20:25:40 +0700

    format

--- python_src/day10/day10_test.py
+++ python_src/day10/day10_test.py
@@ -8,7 +8,9 @@ from python_src.common.call_advent_function import get_payload_result
 STORE, INSTANCE = load_wasm_module("./emcc_wasm/build/mod.wasm")
 
 fewest_button_presses = INSTANCE.exports(STORE)["fewest_button_presses"]
-fewest_presses_to_configuration = INSTANCE.exports(STORE)["fewest_presses_to_configuration"]
+fewest_presses_to_configuration = INSTANCE.exports(STORE)[
+    "fewest_presses_to_configuration"
+]
 
 
 def test_day10_pt1_sample():
